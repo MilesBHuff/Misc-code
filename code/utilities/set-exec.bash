a8a41c4ab933f46b8b11b9f3a172ad5eebf0b997
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2019-06-09 22:23:43 -0400

    Added a script to set the exec bit on executables

---
+++ code/utilities/set-exec.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## Copyright © by Miles Bradley Huff from 2019 per the LGPL3 (the Third Lesser GNU Public License)
+## Sets the executable bit on all files with matching extensions at or below this directory
+find . -type f |\
+while read F; do
+	if [[ -z $(echo "$F" | sed 's/^.*\.\(?:bash\|exe\|msi\|ps1\|py\|sh\)$//') ]]; then
+		echo "$F"
+	fi
+done |\
+while read L; do
+	chmod +x -c "$L"
+done
