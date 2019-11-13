8fd8054e540fd704d45c2852b942f967f6398946
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2019-11-13 14:04:54 -0500

    Moved some scripts into my AwesomeWM repo

--- code/wrappers/virtualbox.bash
+++
@@ -1,7 +0,0 @@
-#/usr/bin/env bash
-## Copyright © by Miles Bradley Huff from 201? per the LGPL3 (the Third Lesser GNU Public License)
-if [ ! -f /tmp/vbox-dkms.lck ]; then
-    gksudo modprobe vboxdrv vboxnetadp vboxnetflt
-    cat "" > /tmp/vbox-dkms.lck
-fi
-VirtualBox %U
