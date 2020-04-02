85a06c0e201a0e6086f1316f504c521eb0f2c18b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-04-01 21:19:00 -0400

    Committing Bannerlord setup script I wrote last night

---
+++ code/setup/wine/bannerlord-proton-setup.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+## For Bannerlord e1.0.1
+set -e
+
+## Find steamapps (necessary in case the game is installed on another drive)
+if [[ ! -z $1 ]]; then
+    STEAMAPPS="$1"
+else
+    STEAMAPPS="~/.steam/steam/steamapps"
+fi
+
+## Symlink EXEs (Thanks to https://www.protondb.com/app/261550#txbO8mhLD2 for the idea.)
+cd "$STEAMAPPS/common/Mount & Blade II Bannerlord/bin/Win64_Shipping_Client"
+ln -sfv 'Bannerlord_BE.exe' 'ManagedStarter_BE.exe'
+ln -sfv 'Bannerlord.exe'    'ManagedStarter.exe'
+
+## Change settings (Thanks to https://www.protondb.com/app/261550#3W0VKwtBY9 for the config path.)
+cd "$STEAMAPPS/compatdata/261550/pfx/drive_c/users/steamuser/My Documents/Mount and Blade II Bannerlord/Configs"
+FILE='engine_config.txt'
+sed -i 's/^\(brightness_calibrated = \)0/\11/m' "$FILE" ## You'll get stuck on this screen if you aren't using a gamepad.
+sed -i 's/^\(cheat_mode = \)0/\11/m'            "$FILE" ## It's extremely buggy -- you need cheats enabled to work around issues.
+sed -i 's/^\(lighting_quality = \)2/\11/m'      "$FILE" ## Caused ghosting if set higher than 'Medium' during Beta; not sure if fixed. (I wrote about it here:  https://forums.taleworlds.com/index.php?threads/b0-2-8-weird-overlay-in-battles.388605/post-9174714)
+
+## One person used these ase their launch options, but they don't seem to do much of anything useful in my tests. (https://www.protondb.com/app/261550#jgkbMLpstU)
+# PROTON_NO_ESYNC=1 gamemoderun %command% +in_terminal 1 +com_skipIntroVideo 1 +com_skipKeyPressOnLoadScreens 1 +com_skipSignInManager 1
