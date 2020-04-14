da916c73cb2b48747e53656f99d8b6c91fa7f62a
Miles B Huff <MilesBHuff@users.noreply.github.com>
2020-04-14 11:24:00 -0400

    Commented/removed code not needed for the current version of M&B

--- code/setup/wine/config-bannerlord-for-linux.bash
+++ code/setup/wine/config-bannerlord-for-linux.bash
@@ -15,9 +15,6 @@ ln -sfv 'Bannerlord_BE.exe' 'ManagedStarter_BE.exe'
 ln -sfv 'Bannerlord.exe'    'ManagedStarter.exe'
 
 ## Change settings (Thanks to https://www.protondb.com/app/261550#3W0VKwtBY9 for the config path.)
-cd "$STEAMAPPS/compatdata/261550/pfx/drive_c/users/steamuser/My Documents/Mount and Blade II Bannerlord/Configs"
-FILE='engine_config.txt'
-sed -i 's/^\(brightness_calibrated = \)0/\11/m' "$FILE" ## You'll get stuck on this screen if you aren't using a gamepad.
-sed -i 's/^\(cheat_mode = \)0/\11/m'            "$FILE" ## It's extremely buggy -- you need cheats enabled to work around issues.
-sed -i 's/^\(lighting_quality = \)2/\11/m'      "$FILE" ## Caused ghosting if set higher than 'Medium' during Beta; not sure if fixed. (I wrote about it here:  https://forums.taleworlds.com/index.php?threads/b0-2-8-weird-overlay-in-battles.388605/post-9174714)
-echo 'Settings updated.'
+#cd "$STEAMAPPS/compatdata/261550/pfx/drive_c/users/steamuser/My Documents/Mount and Blade II Bannerlord/Configs"
+#FILE='engine_config.txt'
+#echo 'Settings updated.'
